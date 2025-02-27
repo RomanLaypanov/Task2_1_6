b85537d7b2c1944feb5ebdfbd49ddbc7e374d2b1
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-02-27 14:49:05 +0300

    Задача 2.1.6 третия проверка

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public static void main(String[] args) {
-
         Cat.sayHello();
         Dog.sayHello();
-        Dog.catchCat();
+        Dog.catchCat(new Cat());
+
 
     }
 
@@ -19,7 +19,7 @@ public class Solution {
             System.out.println("Гав");
         }
 
-        public static void catchCat() {
+        public static void catchCat(Cat cat) {
             System.out.println("Кошка поймана");
             Dog.sayHello();
             Cat.sayHello();
